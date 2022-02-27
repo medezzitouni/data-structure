48900fc0c6731838f6f49958d117b59b5d0278ce
methe ezzitouni <methe@walo.app>
2022-02-27 22:03:23 +0100

    enhanced the stack function

--- src/stack/Stack.js
+++ src/stack/Stack.js
@@ -33,21 +33,57 @@ export default class Stack{
        }
     }
 
+    /**
+     * @name pop
+     * @description pop (return and delete) the last element pushed to the satck
+     *
+     * 
+     * @returns {any}
+     */
     pop(){
         if(this.isEmpty()) return null;
         let ret = this.list.head.val;
         this.list.delete(1);
         return ret;
     }
+
+    /**
+     * @name peek
+     * @description peek, return the last element pushed to the satck
+     *   
+     * 
+     * @returns {any}
+     */
     peek(){
         return this.isEmpty() ? null : this.list.head.val;
     }
+
+     /**
+     * @name isEmpty
+     * @description check the stack if it's empty
+     *   
+     * 
+     * @returns {boolean}
+     */
     isEmpty(){
         return !this.list.head && !this.list.length
     }
+
+     /**
+     * @name isFull
+     * @description check if it's full 
+     *   
+     * 
+     * @returns {boolean}
+     */
     isFull(){
         // we don't have a limit for the number of elemements we can push to the stack.
     }
+
+     /**
+     * @name display
+     * @description display the stack 
+     */
     display(){
         this.list.display()
     }
